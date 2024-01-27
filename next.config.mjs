e5479127257e5eff5fb1bdb6dcd8c7cead151665
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2024-01-27 14:30:46 -0800

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,15 @@ const nextConfig = {
    */
   output: "export",
 
-  distDir: 'out',
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;

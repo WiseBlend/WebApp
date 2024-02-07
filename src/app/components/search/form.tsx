4afef0b7fc040fbd53b227178090f40ba8b3337c
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2024-02-07 10:17:28 -0800

    Add image upload

--- src/app/components/search/form.tsx
+++ src/app/components/search/form.tsx
@@ -10,22 +10,41 @@ const API_ENDPOINT = "/dupes/search";
 
 export default function SearchForm() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const imageRef = useRef<HTMLInputElement>(null);
   const { setProducts } = useContext(SearchContext);
 
   const search = () => {
     const input = inputRef.current;
+    const image = imageRef.current;
     const host = location.hostname === "localhost" ? API_URL_DEV : API_URL_PROD;
     const endpoint = host + API_ENDPOINT + "?url=";
     const url = input && input.value.trim();
+    const img = image && image.files && image.files[0];
+
+    if (input) input.value = "Loading...";
+
+    const reset = () => {
+      if (input) input.value = "";
+      if (image) image.value = "";
+    };
 
     if (url) {
       fetch(endpoint + encodeURIComponent(url))
         .then((res) => res.json())
         .then((data) => {
           setProducts(data.dupes || []);
+          reset();
           input.value = url;
         });
-      input.value = "Loading...";
+    } else if (img) {
+      const data = new FormData();
+      data.append("image", img);
+      fetch(endpoint, { method: "POST", body: data })
+        .then((res) => res.json())
+        .then((data) => {
+          setProducts(data.dupes || []);
+          reset();
+        });
     }
   };
 
@@ -34,9 +53,15 @@ export default function SearchForm() {
       <input
         ref={inputRef}
         type="url"
-        required
         className="grow px-4 rounded-l-2xl"
         placeholder="Enter product URL"
+      />
+      <input
+        ref={imageRef}
+        type="file"
+        className="pr-2"
+        role="button"
+        onChange={search}
       />
       <button type="submit" className="rounded-r-2xl"></button>
     </form>
